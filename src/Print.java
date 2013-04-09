3843cb5ac6f0d46fb17fb1c57b11985e18c20e85
AdrianDaraba <adrian.daraba26@gmail.com>
2013-04-09 09:48:56 +0300

    updateAdrian PrintStuff.java
    This version is better

--- src/Print.java
+++ src/Print.java
@@ -2,9 +2,10 @@ public class Print {
 
 	public static void main(String[]args){
 		
-		System.out.println("1 + 1 = 2");
+		System.out.println("1 + 5 = 2");
 		System.out.println("2 + 2 = 4");
 		System.out.println("3 + 3 = 4");
+		System.out.println("Monkey :D");
 		
 	}
 }
